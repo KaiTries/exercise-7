2ca94973a9d8b4fde8ac0520ebbde3c11fae13b0
KaiTries <kai.schultz@bluewin.ch>
2024-04-22 15:26:53 +0200

    new

--- ant.py
+++ ant.py
@@ -1,27 +1,68 @@
+import random
 # Class representing an artificial ant of the ant colony
 """
     alpha: a parameter controlling the influence of the amount of pheromone during ants' path selection process
     beta: a parameter controlling the influence of the distance to the next node during ants' path selection process
 """
 class Ant():
-    def __init__(self, alpha: float, beta: float, initial_location: int):
+    def __init__(self, alpha: float, beta: float, initial_location):
         self.alpha = alpha
         self.beta = beta
         self.current_location = initial_location
+        self.path = []
         self.travelled_distance = 0
 
-       # The ant runs to visit all the possible locations of the environment 
+    # The ant runs to visit all the possible locations of the environment 
     def run(self):
-        pass
+        # start at first node
+        self.path = [self.current_location]
+        self.travelled_distance = 0
 
+        # create a set of nodes not yet visited
+        not_yet_visited = set(range(48)) - set(self.path)
         
-                
+        # while there are still nodes left to explore
+        while not_yet_visited:
+            # select next node
+            next_node = self.select_path(not_yet_visited)
+            # add distance to that node to the total cost
+            self.travelled_distance += self.get_distance(next_node)
+            # remove the node from the set
+            not_yet_visited.remove(next_node)
+            # add it to the tour
+            self.path.append(next_node)
+        
+        # add the distance from last node back to first node
+        self.travelled_distance += self.get_distance(self.path[0])
+        # add first node again
+        self.path.append(self.path[0])
+
+
     # Select the next path based on the random proportional rule of the ACO algorithm
-    def select_path(self):
-        pass
+    def select_path(self, not_yet_visited):
+
+        pheromone_map = self.environment.pheromones
+        distances_map = self.environment.distances
+
+        probabilites = []
+        divisor = 0
+
+        for city in not_yet_visited:
+            pheromone_level = pheromone_map[self.current_location][city]
+            distance_heuristic = distances_map[self.current_location][city]
+
+            probability = pheromone_level**self.alpha * distance_heuristic**self.beta
+            divisor += pheromone_level**self.alpha * distance_heuristic**self.beta
+            probabilites.append(probability)
+
+        for i in range(len(probabilites)):
+            probabilites[i] /= divisor
+        
+        return random.choices(list(not_yet_visited),weights=probabilites,k=1)[0]
 
     # Position an ant in an environment
     def join(self, environment):
         self.environment = environment
 
-
+    def get_distance(self,to):
+        return self.environment.distances[self.current_location][to]
--- environment.py
+++ environment.py
@@ -1,88 +1,79 @@
 import math
-import random
 import tsplib95
+import numpy as np
 
 # Class representing the environment of the ant colony
 """
     rho: pheromone evaporation rate
 """
 class Environment:
-    def __init__(self,tsp: str, rho: float, ant_population: int):
-        self.tsp = tsp
-        self.rho = rho
+    def __init__(self, rho, ant_population):
+
+        self.rho =rho
         self.ant_population = ant_population
+        # Initialize the environment topology
+        self.problem = tsplib95.load("./att48-specs/att48.tsp")
+        
+        self.distances = np.full((self.problem.dimension, self.problem.dimension), 0)
+        self.pheromones = None
 
-        # initialize env topology
-        self.problem = tsplib95.load(tsp)
-        self.nodes = list(self.problem.get_nodes())
-        self.edges = self.problem.get_edges()
-
-        self.pheromone_map = {}
-        self.distance_map = {}
-
-        # initialize phereomone map
+        # Intialize the pheromone map in the environment
         self.initialize_distance_map()
         self.initialize_pheromone_map()
 
-        self.update_pheromone_map(None)
-
-    
     def initialize_distance_map(self):
-        for edge in self.edges:
-            self.distance_map[(edge[0],edge[1])] = self._get_distance(edge[0],edge[1])
+        for i in range(self.problem.dimension):
+            for j in range(self.problem.dimension):
+                if i != j:
+                    self.distances[i][j] = self.problem.get_weight(i+1,j+1)
 
     # Intialize the pheromone trails in the environment
-    def initialize_pheromone_map(self) -> None:
-        curr_node = random.randint(1,48)
+    def initialize_pheromone_map(self):
+        current_node = 0
         cost = 0
         visited = []
 
-        while (len(visited) < 48):
-            visited.append(curr_node)
+        while len(visited) < 48:
+            visited.append(current_node)
             closest = float("inf")
             next_node = None
-            for n in range(1, 48):
+            for n in range(self.problem.dimension):
                 if n not in visited:
-                    dist = self.get_distance(curr_node,n)
+                    dist = self.distances[current_node][n]
                     if dist < closest:
                         closest = dist
                         next_node = n
                         cost += dist
             if next_node:
-                curr_node = next_node
-
+                current_node = next_node
+        
         visited.append(visited[0])
-        cost += self.get_distance(curr_node, visited[0])
+        cost += self.distances[current_node][visited[0]]
+        
+        
+        print("initial pheromone per trail: ", self.ant_population / cost)
 
-        print("initial pheromone per edge: ", self.ant_population / cost)
+        self.pheromones = np.full((self.problem.dimension, self.problem.dimension), self.ant_population / cost)
 
-        for edge in self.edges:
-            self.pheromone_map[(edge[0],edge[1])] = self.ant_population / cost
-            
+        
 
     # Update the pheromone trails in the environment
-    def update_pheromone_map(self, ants: list) -> None:
-        for edge in self.pheromone_map:
-            self.pheromone_map[edge] *= (1 - self.rho)
+    def update_pheromone_map(self, ants: list):
+        # Step 1: Evaporate
+        self.pheromones *= (1 - self.rho)
 
+        # Step 2: Deposit
         for ant in ants:
-            for c in range(len(ant.visited) - 1):
-                i = ant.visited[c]
-                j = ant.visited[c + 1]
-                self.pheromone_map[(i,j)] += 1 / self.get_distance(i,j)
+            for i in range(len(ant.path)-1):
+                self.pheromones[ant.path[i]][ant.path[i + 1]] += 1 / ant.travelled_distance
+                self.pheromones[ant.path[i + 1]][ant.path[i]] += 1 / ant.travelled_distance
         
     # Get the pheromone trails in the environment
     def get_pheromone_map(self):
-        pass
+        return self.pheromones
+    
+    def get_distance_map(self):
+        return self.distances
+    
 
-    # Get the environment topology
-    def get_possible_locations(self, current_location: int):
-        pass
-
-    # Get the pseudo-euclidean distance between two vertices
-    def get_distance(self, i, j):
-        return self.distance_map[(i,j)]
-
-
-    def _get_distance(self, i: int, j: int) -> int:
-        return tsplib95.distances.pseudo_euclidean(self.problem.node_coords[i],self.problem.node_coords[j],round=math.ceil)
+    
